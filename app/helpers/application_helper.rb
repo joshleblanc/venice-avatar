0d0fcbf7707c2ea334c2cd6bf67c33f76a2421df
Joshua LeBlanc <jleblanc@hey.com>
2025-07-06 08:08:52 -0300

    feat: italicize actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def format_message_content(content)
+    return content if content.blank?
+    
+    # Convert text between single asterisks to italics
+    # e.g., "Hello *waves* there!" becomes "Hello <em>waves</em> there!"
+    formatted_content = content.gsub(/\*([^*]+)\*/) do |match|
+      "<em>#{$1.strip}</em>"
+    end
+    
+    # Return as HTML safe string so it renders properly
+    formatted_content.html_safe
+  end
 end
