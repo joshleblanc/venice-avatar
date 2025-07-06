d48c5a7ebcb20e64b386547675dc36ab15aa3d5d
Joshua LeBlanc <jleblanc@hey.com>
2025-07-06 08:09:19 -0300

    chore: color actions too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def format_message_content(content)
     return content if content.blank?
-    
+
     # Convert text between single asterisks to italics
     # e.g., "Hello *waves* there!" becomes "Hello <em>waves</em> there!"
     formatted_content = content.gsub(/\*([^*]+)\*/) do |match|
-      "<em>#{$1.strip}</em>"
+      "<em style='color: purple;'>#{$1.strip}</em>"
     end
-    
+
     # Return as HTML safe string so it renders properly
     formatted_content.html_safe
   end
